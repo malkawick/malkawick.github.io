358c91b345d972c9861981caac85a6768c89699a
malkawick <shkrishanthi@gmail.com>
2026-03-01 17:28:49 +0000

    Deploying to gh-pages from @ malkawick/malkawick.github.io@9618671ce0143178eaf918e906977f79c9d61352 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,7 +451,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "teachings-data-science-fundamentals",
+          section: "News",},{id: "projects-project-phoenix-cubesat",
+          title: 'Project Phoenix (CubeSat)',
+          description: "Ground Station UI and LoRA communication for a high-altitude weather balloon.",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/1_phoenix/";
+            },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
           section: "Teachings",handler: () => {
